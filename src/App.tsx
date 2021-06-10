e8ae6e8abe7e4edfdfd4eb19223c0ad3766ee641
lergnom <lergnom@mail.ru>
2021-06-10 13:55:57 +0300

    TodoList #07 - ok

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import './App.css';
 import {Todolist} from "./Todolist";
 import {v1} from 'uuid';
 import {AddItemForm} from "./AddItemForm";
+import {AppBar, Button, Container, Grid, IconButton, Paper, Toolbar, Typography} from "@material-ui/core";
+import {Menu} from "@material-ui/icons";
 
 type  TaskType = {
     id: string
@@ -138,27 +140,51 @@ function App() {
         }
 
         return (
-            <Todolist key={tl.id} tlID={tl.id} title={tl.title} tasks={tasksForTodoList} removeTask={removeTask}
-                      addTask={addTask}
-                      changeFilter={changeFilter} checkBox={checkBox} filter={tl.filter} removeList={removeList}
-                      onChangeTitle={onChangeTitle} onChangeTodoListTitle={onChangeTodoListTitle}/>
+            <Grid item>
+                <Paper style={{padding: "10px"}}>
+                    <Todolist key={tl.id} tlID={tl.id} title={tl.title} tasks={tasksForTodoList} removeTask={removeTask}
+                              addTask={addTask}
+                              changeFilter={changeFilter} checkBox={checkBox} filter={tl.filter} removeList={removeList}
+                              onChangeTitle={onChangeTitle} onChangeTodoListTitle={onChangeTodoListTitle}/>
+                </Paper>
+            </Grid>
+
         )
 
 
     })
 
     return (
-        <
-            div
+        <div
             className="App">
-            < AddItemForm
-                addItem={addTodoList}
-            />
-            {
-                todoListJsxElement
-            }
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton edge="start" color="inherit" aria-label="menu">
+                        <Menu/>
+                    </IconButton>
+                    <Typography variant="h6">
+                        News
+                    </Typography>
+                    <Button color="inherit">Login</Button>
+                </Toolbar>
+            </AppBar>
+            <Container fixed>
+                <Grid container style={{padding: "20px"}}>
+                    <AddItemForm
+                        addItem={addTodoList}
+                    />
+                </Grid>
+                <Grid container spacing={3}>
+                    {
+                        todoListJsxElement
+                    }
+                </Grid>
+
+            </Container>
+
         </div>
-    );
+    )
+        ;
 }
 
 export default App;
